e447933d70d2a021ae3f62b26766c21d40bed63f
nishtha <1834810005.cse.psitcoe@gmail.com>
2022-09-27 18:25:27 +0530

    commit

---
+++ backend/config/env.js
@@ -0,0 +1,10 @@
+
+const env = {
+  database: "products",
+  username: "myserver@myserver12367",
+  password: "jinvjung@123A",
+  host: "myserver12367.mysql.database.azure.com",
+  dialect: "mysql",
+};
+
+module.exports = env;
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://backendwebapp2.azurewebsites.net'
+  
+};
